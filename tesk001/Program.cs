a407e5b11b3d7e654e4b9b6b6fc0be843f639fdd
Roman <roma.klimov10.94@gmail.com>
2023-04-18 09:16:50 +1100

    Сохранение

--- tesk001/Program.cs
+++ tesk001/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача: Написать программу которая выводит случайное число из отрезка (чисел) [10, 99] и показывает наибольшую цифру числа .
+
+например:
+8→ 8
+
+12→2
+
+85→8
+*/
+
+
+/*Console.Clear();
+
+void [] Sort(int[] numbers, bool reverse = false)
+{
+    Console.WriteLine($"{name}");
+}
+  int[] mass1 = {1, 2, 5, 7, 9, 5,};
+  Sort(mass1, reverse : true);  
+
+  int[] array = new int [4];
+
+  */
+
+  int x = 20; 
+
+  if (x > 10)
+  {
+    Console.WriteLine("+++");
+  }
+  else
+  {
+    Console.WriteLine("---");
+  }
