644f4b6160f8422ae560a88f9d13794470aad8f7
Roman <roma.klimov10.94@gmail.com>
2023-04-18 23:06:03 +1100

    Создал прогрмму которая убирает 2у цифру из трехзначного числа

--- tesk011_dellTwoDigit/Program.cs
+++ tesk011_dellTwoDigit/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿/* Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+
+int GetRandomInt(int arg1, int arg2)
+{
+    return new Random().Next(arg1, arg2 + 1);
+    
+}
+
+int GetDigit13(int number)
+{
+    int digit3 = (number % 10);
+    int digit1 = (number / 100)*10;
+
+    int resolt = digit1 + digit3;
+    return resolt;
+}
+
+
+int number = GetRandomInt(100, 999);
+Console.WriteLine(number);
+int maxDigit = GetDigit13(number);
+Console.WriteLine(maxDigit);
