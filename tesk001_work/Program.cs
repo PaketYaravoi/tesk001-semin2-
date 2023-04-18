630253343763fcaea4fd1b91661ceae3853195fa
Roman <roma.klimov10.94@gmail.com>
2023-04-18 13:58:58 +1100

    Создал функцию для поиска случайного числа из отрезка (tesk001_work)

--- tesk001_work/Program.cs
+++ tesk001_work/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача: Написать программу которая выводит случайное число из отрезка (чисел) [10, 99] и показывает наибольшую цифру числа .
+
+например:
+8→ 8
+
+12→2
+
+85→8
+*/
+Console.Clear();
+int GetRandomInt()
+{
+    // int random = new Random().Next(10, 100);
+    // return random;
+    return new Random().Next(10, 100);
+}
+Console.WriteLine(GetRandomInt());
+Console.WriteLine(GetRandomInt());
+Console.WriteLine(GetRandomInt());
+Console.WriteLine(GetRandomInt());
